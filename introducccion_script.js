3b140d0921f53adccc231cc6f4babfdef100cb59
dannycornelio <licdannycornelio@gmail.com>
2023-07-04 21:56:09 -0600

    dato booleano

--- introducccion_script.js
+++ introducccion_script.js
@@ -1,7 +1,15 @@
 //undefine
+
 let x;
 let y;
+
 console.log(x);
 console.log(y);
 
-//
+//Booleano
+
+let numero = 10;
+let numero1 = 5;
+
+console.log(numero <= numero1)
+console.log(numero >= numero1)
