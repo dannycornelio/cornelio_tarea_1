3676c9a4f9f1357ab4242273f41dd9ab9957bb96
dannycornelio <licdannycornelio@gmail.com>
2023-07-07 20:30:13 -0600

    creacion de las variables

--- introducccion_script.js
+++ introducccion_script.js
@@ -5,11 +5,37 @@ let y;
 
 console.log(x);
 console.log(y);
+//alert(x)
+//alert(y)
 
 //Booleano
 
-let numero = 10;
-let numero1 = 5;
+let bool1 = 10;
+let bool2 = 5;
 
-console.log(numero <= numero1)
-console.log(numero >= numero1)
+console.log(bool1 <= bool2);
+console.log(bool1 >= bool2);
+
+
+//numerico
+
+let numero = 10
+
+//alert(numero);
+console.log(numero)
+
+
+//cadena de texto 
+let nombre = "Danny" 
+//alert("Hola, me llamo "+nombre+"!");
+console.log("Hola, me llamo "+nombre+"!")
+
+
+//Null
+
+let variableNula = null
+
+//alert(variableNula)
+console.log(variableNula)
+
+
